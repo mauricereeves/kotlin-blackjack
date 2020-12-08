7205cc50688d9fd66c87fb586bf0222d714a9d2d
Maurice Reeves <maurice.reeves@gmail.com>
2020-12-08 15:38:02 -0500

    More refactoring. Always refactoring

---
+++ src/HiLoCount.kt
@@ -0,0 +1,26 @@
+package blackjack
+
+class HiLoCount : ICardCountingSystem {
+    override var count: Int = 0
+
+    override fun addToCount(card: Card): Int {
+        when (card.value) {
+            Value.TWO,
+            Value.THREE,
+            Value.FOUR,
+            Value.FIVE,
+            Value.SIX -> count += 1
+
+            Value.TEN,
+            Value.JACK,
+            Value.QUEEN,
+            Value.KING,
+            Value.ACE -> count -= 1
+
+            // 7 - 9 the count remains stable so do nothing
+            else -> count += 0
+        }
+
+        return count
+    }
+}
--- src/game.kt
+++ src/game.kt
@@ -53,31 +53,6 @@ class Hand {
     }
 }
 
-class HiLoCount : ICardCountingSystem {
-    override var count: Int = 0
-
-    override fun addToCount(card: Card): Int {
-        when (card.value) {
-            Value.TWO,
-            Value.THREE,
-            Value.FOUR,
-            Value.FIVE,
-            Value.SIX -> count += 1
-
-            Value.TEN,
-            Value.JACK,
-            Value.QUEEN,
-            Value.KING,
-            Value.ACE -> count -= 1
-
-            // 7 - 9 the count remains stable so do nothing
-            else -> count += 0
-        }
-
-        return count
-    }
-}
-
 /**
  *
  */
@@ -107,7 +82,7 @@ class Deck : ICardHolder {
     }
 }
 
-class Shoe(val countOfDecks: Int = 4) : ICardHolder {
+class Shoe(private val countOfDecks: Int = 4) : ICardHolder {
     override val cards: MutableList<Card> = mutableListOf<Card>()
 
     init {
