40fdb633bdfeb112f0cf02d3cc31e0d800a4bd9b
Maurice Reeves <maurice.reeves@ssa.gov>
2025-02-13 09:01:49 -0500

    Added gradle build file and a gitignore which I should have done a long time ago.

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(23)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("gameKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
